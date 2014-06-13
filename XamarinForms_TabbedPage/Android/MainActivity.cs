85072494678bfd22bc1800c15badb5cf2e58bb5e
ytabuchi <ytabuchi.xlsoft@gmail.com>
2014-06-13 18:32:07 +0900

    TabbedPage added

---
+++ XamarinForms_TabbedPage/Android/MainActivity.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+using Xamarin.Forms.Platform.Android;
+
+
+namespace XamarinForms_TabbedPage.Android
+{
+	[Activity (Label = "XamarinForms_TabbedPage.Android.Android", MainLauncher = true)]
+	public class MainActivity : AndroidActivity
+	{
+		protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+
+			Xamarin.Forms.Forms.Init (this, bundle);
+
+			SetPage (App.GetMainPage ());
+		}
+	}
+}
+
---
+++ XamarinForms_TabbedPage/XamarinForms_TabbedPage/App.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace XamarinForms_TabbedPage
+{
+	public class App
+	{
+		public static Page GetMainPage ()
+		{	
+
+			var tabPage1 = new ContentPage () { 
+				Title = "Page 1",
+				Icon = "1.png",
+				Content = new Label { 
+					Text = "Page 1 Label",
+					HorizontalOptions = LayoutOptions.Center,
+					VerticalOptions = LayoutOptions.Center,
+				},
+			};
+
+			var label = new Label { 
+				Text = "Page 2 Label",
+				TextColor = Color.White,
+				HorizontalOptions = LayoutOptions.Center,
+			};
+
+			var button = new Button {
+				Text = "Page Name",
+				HorizontalOptions = LayoutOptions.CenterAndExpand,
+			};
+
+			var tabPage2 = new ContentPage () { 
+				Title = "Page 2",
+				Icon = "2.png",
+				Content = new StackLayout {
+					Children = {
+						label,
+						button,
+					},
+					HorizontalOptions = LayoutOptions.Center,
+					VerticalOptions = LayoutOptions.Center,
+				},
+				BackgroundColor = Color.FromHex("666666"),
+			};
+
+			var mainPage = new TabbedPage { 
+				Children = {
+					tabPage1,
+					tabPage2,
+				},
+			};
+
+			button.Clicked += (sender, e) => {
+				mainPage.DisplayAlert("Alert Title", mainPage.CurrentPage.Title + " is selected.", "OK", "Cancel");
+			};
+
+			return mainPage;
+
+		}
+	}
+}
+
